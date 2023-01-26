24c62b3e7da3af4c80b53b696ec8c87460ac229e
Mansi Srivastav <94063298+mansi23-01@users.noreply.github.com>
2023-01-26 11:53:43 +0530

    Update CyclicallyRotateAnArray.cpp

--- CyclicallyRotateAnArray.cpp
+++ CyclicallyRotateAnArray.cpp
@@ -11,3 +11,27 @@ void rotate(int arr[], int n)
     arr[0]=temp;
     
 }
+
+//////////////////////////
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+   for(int i=0; i<n;i++){
+      cin>>arr[i];
+  }
+  //int arr[5]={3,2,5,4,1};
+
+    int temp=arr[n-1];
+  for(int i=n-1;i>=0;i--){
+      arr[i]=arr[i-1];
+  }
+  arr[0]=temp;
+  
+  for(int i=0; i<n;i++){
+      cout<<arr[i];
+  }
+}
